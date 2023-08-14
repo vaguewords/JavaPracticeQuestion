d5cf2ee2e9a07d0bf4f7150f2304fd346b645301
vaguewords <vaguewords@gmail.com>
2023-08-14 18:50:30 +0900

    asdf

--- chap05-array-practice/src/main/java/com/ohgiraffers/section01/array/level02/normal/Application4.java
+++ chap05-array-practice/src/main/java/com/ohgiraffers/section01/array/level02/normal/Application4.java
@@ -1,8 +1,31 @@
 package com.ohgiraffers.section01.array.level02.normal;
 
+import java.util.Scanner;
+
 public class Application4 {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("정수를 입력하세요.");
+        int scannedSize = sc.nextInt();
+        int[] scannedSizeArr = new int[scannedSize];
+        System.out.println("배열 0번째 인덱스에 넣을 값을 입력하세요.");
+        int scannedVariable0 = sc.nextInt();
+        System.out.println("배열 1번째 인덱스에 넣을 값을 입력하세요.");
+        int scannedVariable1 = sc.nextInt();
+        System.out.println("배열 2번째 인덱스에 넣을 값을 입력하세요.");
+        int scannedVariable2 = sc.nextInt();
+
+        scannedSizeArr = new int[]{scannedVariable0, scannedVariable1, scannedVariable2};
+
+        for (int i=0; i < scannedSizeArr.length; i++) {
+            System.out.print(scannedSizeArr[i] + " ");
+
+        }
+        System.out.println();
+        System.out.println("배열 길이: " + scannedSizeArr.length);
+        System.out.println("값 총합: " + scannedVariable0 + scannedVariable1 + scannedVariable2);
 
         /* 사용자가 입력한 배열의 길이만큼의 정수형 배열을 선언 및 할당하고
          * 배열 인덱스 넣을 값 역시 사용자가 입력하여 배열을 초기화히세요.
--- chap05-array-practice/src/main/java/com/ohgiraffers/section01/array/level02/normal/Application5.java
+++ chap05-array-practice/src/main/java/com/ohgiraffers/section01/array/level02/normal/Application5.java
@@ -1,8 +1,33 @@
 package com.ohgiraffers.section01.array.level02.normal;
 
+import java.util.Scanner;
+
 public class Application5 {
 
     public static void main(String[] args) {
+
+        String[] chickenArr = new String[]{"후라이드", "양념", "파닭", "매콤", "치즈", "간장"};
+
+        Scanner sc = new Scanner(System.in);
+        String chickenSearch = sc.nextLine();
+
+        switch (chickenSearch) {
+            case "후라이드" :
+                System.out.println("후라이드 치킨 배달 가능"); break;
+            case "양념" :
+                System.out.println("양념 치킨 배달 가능"); break;
+            case "파닭" :
+                System.out.println("파닭 치킨 배달 가능"); break;
+            case "매콤" :
+                System.out.println("매콤 치킨 배달 가능"); break;
+            case "치즈" :
+                System.out.println("치즈 치킨 배달 가능"); break;
+            case "간장" :
+                System.out.println("간장 치킨 배달 가능"); break;
+            default:
+                System.out.println(chickenSearch + " 치킨은 없는 메뉴입니다");
+                
+        }
 
         /* 후라이드, 양념, 파닭, 매콤, 치즈, 간장이라는 메뉴 치킨 메뉴를
          * 배열에 초기화하고, 사용자가 입력한 값이 배열에 있는지 검색하여
