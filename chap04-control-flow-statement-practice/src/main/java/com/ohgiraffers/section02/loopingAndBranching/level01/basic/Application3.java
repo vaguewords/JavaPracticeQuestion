0066f6bfcf15aba7d2c73bf66264200cb60ffc51
vaguewords <59818937+vaguewords@users.noreply.github.com>
2023-08-16 19:16:21 +0900

    Update Application3.java

--- chap04-control-flow-statement-practice/src/main/java/com/ohgiraffers/section02/loopingAndBranching/level01/basic/Application3.java
+++ chap04-control-flow-statement-practice/src/main/java/com/ohgiraffers/section02/loopingAndBranching/level01/basic/Application3.java
@@ -1,9 +1,22 @@
 package com.ohgiraffers.section02.loopingAndBranching.level01.basic;
 
+import java.util.Scanner;
+
 public class Application3 {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("정수 입력");
+        int scannedNum = sc.nextInt();
+        int evenSum = 0;
+
+        for(int i = 1; i <= scannedNum; i++) {
+            if ((i + 1) % 2 == 0) {
+                evenSum = evenSum + (i + 1);
+            }
+        }
+        System.out.println(evenSum);
         /* 1부터 입력받은 정수까지의 짝수의 합을 구하세요
          *
          * -- 입력 예시 --
